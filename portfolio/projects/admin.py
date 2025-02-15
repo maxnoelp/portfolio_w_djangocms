49cb92df6a9967af0a416e0829b97e2bf241e4c6
Max Nölp <noelpmax@gmail.com>
2025-02-15 23:23:58 +0100

    register in admin new model

--- portfolio/projects/admin.py
+++ portfolio/projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from portfolio.projects.models import Choice, ProjectRelease
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class ProjectReleaseAdmin(admin.ModelAdmin):
+    list_display = ("title", "publish")
+    list_filter = ("title", "publish")
+    search_fields = ("title", "tags__name")
+
+
+admin.site.register(ProjectRelease, ProjectReleaseAdmin)
+admin.site.register(Choice)

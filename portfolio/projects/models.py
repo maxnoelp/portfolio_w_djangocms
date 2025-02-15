ebab334d03611f5b783ff7717679a9e199d8af88
Max Nölp <noelpmax@gmail.com>
2025-02-15 23:08:02 +0100

    run makemigrations

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -15,8 +15,10 @@ class ProjectRelease(models.Model):
     publish = models.BooleanField(_("Veröffentlicht"), default=False)
     title = models.CharField(_("Titel"), max_length=100)
     content = HTMLField()
-    tags = models.ManyToManyField(_("Tags auswähle"), Choice)
-    image_thumbnail = FilerImageField(_("Bild einfügen"), on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Choice, verbose_name=_("Tags auswählen"))
+    image_thumbnail = FilerImageField(
+        verbose_name="Bild einfügen", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.title
